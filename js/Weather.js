f45e27c62d6141ef370538f5270eab5530a43a42
huanxue <2759947937@qq.com>
2024-08-29 15:50:39 +0800

    Site updated: 2024-08-29 15:50:39

--- js/Weather.js
+++ js/Weather.js
@@ -1,3 +1,4 @@
+// 正确的jQuery加载方式
 (function() {
     if (!window.jQuery) {
         const script = document.createElement('script');
@@ -15,81 +16,78 @@
 
 function showWeather(weatherType) {
     const container = document.getElementById('weather-container');
-    container.innerHTML = ''; // Clear the container content
+    container.innerHTML = ''; // 清空容器内容
 
     if (weatherType === 'cloud') {
-        container.innerHTML = `<div class="cloud"></div>`;
+        container.innerHTML = '<div class="cloud"></div>';
     } else if (weatherType === 'rain') {
-        container.innerHTML = `
-            <div class="cloud_rain">
-                <div class="rain" style="left: 30px; animation-delay: 0s;"></div>
-                <div class="rain" style="left: 60px; animation-delay: 0.1s;"></div>
-                <div class="rain" style="left: 90px; animation-delay: 0.2s;"></div>
-                <div class="rain" style="left: 120px; animation-delay: 0.3s;"></div>
-                <div class="rain" style="left: 150px; animation-delay: 0.4s;"></div>
-            </div>
-            <div class="puddle puddle1"></div>
-            <div class="puddle puddle2"></div>
-            <div class="puddle puddle3"></div>
-            <div class="puddle puddle4"></div>
-            <div class="puddle puddle5"></div>
-            <div class="puddle puddle6"></div>
-            <div class="puddle puddle7"></div>
-            <div class="puddle puddle8"></div>
-        `;
+        container.innerHTML = 
+            '<div class="cloud_rain">' +
+                '<div class="rain" style="left: 30px; animation-delay: 0s;"></div>' +
+                '<div class="rain" style="left: 60px; animation-delay: 0.1s;"></div>' +
+                '<div class="rain" style="left: 90px; animation-delay: 0.2s;"></div>' +
+                '<div class="rain" style="left: 120px; animation-delay: 0.3s;"></div>' +
+                '<div class="rain" style="left: 150px; animation-delay: 0.4s;"></div>' +
+            '</div>' +
+            '<div class="puddle puddle1"></div>' +
+            '<div class="puddle puddle2"></div>' +
+            '<div class="puddle puddle3"></div>' +
+            '<div class="puddle puddle4"></div>' +
+            '<div class="puddle puddle5"></div>' +
+            '<div class="puddle puddle6"></div>' +
+            '<div class="puddle puddle7"></div>' +
+            '<div class="puddle puddle8"></div>';
     } else if (weatherType === 'snow') {
-        container.innerHTML = `
-            <div class="cloud_snow">
-                <div class="snowflake large" style="left: 30px; animation-delay: 0s;"></div>
-                <div class="snowflake large" style="left: 80px; animation-delay: 0.3s;"></div>
-                <div class="snowflake large" style="left: 120px; animation-delay: 0.5s;"></div>
-                <div class="snowflake large" style="left: 170px; animation-delay: 0.7s;"></div>
-                <div class="snowflake large" style="left: 200px; animation-delay: 0.9s;"></div>
-                <div class="snowflake small" style="left: 40px; animation-delay: 0.5s;"></div>
-                <div class="snowflake small" style="left: 90px; animation-delay: 0.8s;"></div>
-                <div class="snowflake small" style="left: 130px; animation-delay: 0.9s;"></div>
-                <div class="snowflake small" style="left: 160px; animation-delay: 1s;"></div>
-                <div class="snowflake small" style="left: 220px; animation-delay: 0.1s;"></div>
-                <div class="snowman">
-                    <!-- Snowman SVG code -->
-                </div>
-            </div>
-        `;
+        container.innerHTML = 
+            '<div class="cloud_snow">' +
+                '<div class="snowflake large" style="left: 30px; animation-delay: 0s;"></div>' +
+                '<div class="snowflake large" style="left: 80px; animation-delay: 0.3s;"></div>' +
+                '<div class="snowflake large" style="left: 120px; animation-delay: 0.5s;"></div>' +
+                '<div class="snowflake large" style="left: 170px; animation-delay: 0.7s;"></div>' +
+                '<div class="snowflake large" style="left: 200px; animation-delay: 0.9s;"></div>' +
+                '<div class="snowflake small" style="left: 40px; animation-delay: 0.5s;"></div>' +
+                '<div class="snowflake small" style="left: 90px; animation-delay: 0.8s;"></div>' +
+                '<div class="snowflake small" style="left: 130px; animation-delay: 0.9s;"></div>' +
+                '<div class="snowflake small" style="left: 160px; animation-delay: 1s;"></div>' +
+                '<div class="snowflake small" style="left: 220px; animation-delay: 0.1s;"></div>' +
+                '<div class="snowman">' +
+                    '<!-- Snowman SVG code -->' +
+                '</div>' +
+            '</div>';
     } else if (weatherType === 'thunder') {
-        container.innerHTML = `
-            <div class="cloud_thunder">
-                <div class="lightning"></div>
-                <div class="music_symbol"></div>
-                <div class="music_symbol" style="top: 70px; left: 140px;"></div>
-                <div class="music_symbol" style="top: 90px; left: 80px;"></div>
-            </div>
-        `;
+        container.innerHTML = 
+            '<div class="cloud_thunder">' +
+                '<div class="lightning"></div>' +
+                '<div class="music_symbol"></div>' +
+                '<div class="music_symbol" style="top: 70px; left: 140px;"></div>' +
+                '<div class="music_symbol" style="top: 90px; left: 80px;"></div>' +
+            '</div>';
     } else if (weatherType === 'sun') {
-        container.innerHTML = `
-            <div class="cloud_sun">
-                <div class="sun"></div>
-            </div>
-        `;
+        container.innerHTML = 
+            '<div class="cloud_sun">' +
+                '<div class="sun"></div>' +
+            '</div>';
     } else if (weatherType === 'close'){
         container.innerHTML = '';
     }
 }
 
 // 获取用户位置和天气数据
-function fetchLocationAndWeather() {
-    const script = document.createElement('script');
-    script.src = 'https://apis.map.qq.com/ws/location/v1/ip?key=QNWBZ-K24WT-Z4CXP-VWWLJ-YC6FE-UFFVM&output=jsonp&callback=handleLocationResponse';
-    document.body.appendChild(script);
-}
-
-// Callback function to handle JSONP response
-function handleLocationResponse(data) {
-    console.log('Location Data:', data);
-    if (data.status === 0) {
-        const city = data.result.ad_info.city;
-        getWeather(city);
-    } else {
-        console.error('Failed to retrieve location:', data.message);
+async function fetchLocationAndWeather() {
+    try {
+        const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+        const apiUrl = 'https://apis.map.qq.com/ws/location/v1/ip?key=QNWBZ-K24WT-Z4CXP-VWWLJ-YC6FE-UFFVM&output=json';
+        const response = await fetch(proxyUrl + apiUrl);
+        const data = await response.json();
+        console.log('Location Data:', data);
+        if (data.status === 0) {
+            const city = data.result.ad_info.city;
+            getWeather(city);
+        } else {
+            console.error('Failed to retrieve location:', data.message);
+        }
+    } catch (error) {
+        console.error('Location API Error:', error);
     }
 }
 
