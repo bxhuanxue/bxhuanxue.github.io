9d7e97d5487327e2f15a2aa33283e85ec5aac199
huanxue <2759947937@qq.com>
2024-08-29 17:26:49 +0800

    Site updated: 2024-08-29 17:26:49

--- js/Weather.js
+++ js/Weather.js
@@ -1,3 +1,84 @@
+// 显示天气的函数
+function showWeather(weatherType) {
+    const container = document.getElementById('weather-container');
+    container.innerHTML = ''; // 清空容器内容
+
+    if (weatherType === 'cloud') {
+        container.innerHTML = `
+            <div class="cloud"></div>
+        `;
+    } else if (weatherType === 'rain') {
+        container.innerHTML = `
+            <div class="cloud_rain">
+                <div class="rain" style="left: 30px; animation-delay: 0s;"></div>
+                <div class="rain" style="left: 60px; animation-delay: 0.1s;"></div>
+                <div class="rain" style="left: 90px; animation-delay: 0.2s;"></div>
+                <div class="rain" style="left: 120px; animation-delay: 0.3s;"></div>
+                <div class="rain" style="left: 150px; animation-delay: 0.4s;"></div>
+            </div>
+            <div class="puddle puddle1"></div>
+            <div class="puddle puddle2"></div>
+            <div class="puddle puddle3"></div>
+            <div class="puddle puddle4"></div>
+            <div class="puddle puddle5"></div>
+            <div class="puddle puddle6"></div>
+            <div class="puddle puddle7"></div>
+            <div class="puddle puddle8"></div>
+        `;
+    } else if (weatherType === 'snow') {
+        container.innerHTML = `
+            <div class="cloud_snow">
+                <div class="snowflake large" style="left: 30px; animation-delay: 0s;"></div>
+                <div class="snowflake large" style="left: 80px; animation-delay: 0.3s;"></div>
+                <div class="snowflake large" style="left: 120px; animation-delay: 0.5s;"></div>
+                <div class="snowflake large" style="left: 170px; animation-delay: 0.7s;"></div>
+                <div class="snowflake large" style="left: 200px; animation-delay: 0.9s;"></div>
+                <div class="snowflake small" style="left: 40px; animation-delay: 0.5s;"></div>
+                <div class="snowflake small" style="left: 90px; animation-delay: 0.8s;"></div>
+                <div class="snowflake small" style="left: 130px; animation-delay: 0.9s;"></div>
+                <div class="snowflake small" style="left: 160px; animation-delay: 1s;"></div>
+                <div class="snowflake small" style="left: 220px; animation-delay: 0.1s;"></div>
+                <div class="snowman"></div>
+            </div>
+        `;
+    } else if (weatherType === 'thunder') {
+        container.innerHTML = `
+            <div class="cloud_thunder">
+                <div class="lightning"></div>
+                <div class="music_symbol"></div>
+                <div class="music_symbol" style="top: 70px; left: 140px;"></div>
+                <div class="music_symbol" style="top: 90px; left: 80px;"></div>
+            </div>
+        `;
+    } else if (weatherType === 'sun') {
+        container.innerHTML = `
+            <div class="cloud_sun">
+                <div class="sun"></div>
+            </div>
+        `;
+    } else if (weatherType === 'close'){
+        container.innerHTML = '';
+    }
+}
+
+// 根据天气描述确定天气类型的函数
+function determineWeatherType(weatherText) {
+    if (weatherText.includes("雨")) {
+        return 'rain';
+    } else if (weatherText.includes("云")) {
+        return 'cloud';
+    } else if (weatherText.includes("雷")) {
+        return 'thunder';
+    } else if (weatherText.includes("雪")) {
+        return 'snow';
+    } else if (weatherText.includes("晴")) {
+        return 'sun';
+    } else {
+        console.error('未识别的天气类型:', weatherText);
+        return 'unknown';
+    }
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     let weatherShown = false; // 标志变量，用于检查天气效果是否已经呈现
 
@@ -25,23 +106,7 @@ document.addEventListener('DOMContentLoaded', function() {
             success: function(data) {
                 if (data.code === "200") {
                     const weatherText = data.now.text;
-                    let weatherType;
-
-                    // 根据 weatherText 确定天气类型
-                    if (weatherText.includes("雨")) {
-                        weatherType = 'rain';
-                    } else if (weatherText.includes("云")) {
-                        weatherType = 'cloud';
-                    } else if (weatherText.includes("雷")) {
-                        weatherType = 'thunder';
-                    } else if (weatherText.includes("雪")) {
-                        weatherType = 'snow';
-                    } else if (weatherText.includes("晴")) {
-                        weatherType = 'sun';
-                    } else {
-                        console.error('未识别的天气类型:', weatherText);
-                        weatherType = 'unknown';
-                    }
+                    let weatherType = determineWeatherType(weatherText);
 
                     if (weatherType !== 'unknown' && !weatherShown) {
                         showWeather(weatherType);
@@ -57,70 +122,9 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 
-    function showWeather(weatherType) {
-        const container = document.getElementById('weather-container');
-        if (!container) {
-            console.error('Weather container not found!');
-            return;
-        }
-
-        container.innerHTML = ''; // 清空容器内容
-
-        if (weatherType === 'cloud') {
-            container.innerHTML = '<div class="cloud"></div>';
-        } else if (weatherType === 'rain') {
-            container.innerHTML = 
-                '<div class="cloud_rain">' +
-                    '<div class="rain" style="left: 30px; animation-delay: 0s;"></div>' +
-                    '<div class="rain" style="left: 60px; animation-delay: 0.1s;"></div>' +
-                    '<div class="rain" style="left: 90px; animation-delay: 0.2s;"></div>' +
-                    '<div class="rain" style="left: 120px; animation-delay: 0.3s;"></div>' +
-                    '<div class="rain" style="left: 150px; animation-delay: 0.4s;"></div>' +
-                '</div>' +
-                '<div class="puddle puddle1"></div>' +
-                '<div class="puddle puddle2"></div>' +
-                '<div class="puddle puddle3"></div>' +
-                '<div class="puddle puddle4"></div>' +
-                '<div class="puddle puddle5"></div>' +
-                '<div class="puddle puddle6"></div>' +
-                '<div class="puddle puddle7"></div>' +
-                '<div class="puddle puddle8"></div>';
-        } else if (weatherType === 'snow') {
-            container.innerHTML = 
-                '<div class="cloud_snow">' +
-                    '<div class="snowflake large" style="left: 30px; animation-delay: 0s;"></div>' +
-                    '<div class="snowflake large" style="left: 80px; animation-delay: 0.3s;"></div>' +
-                    '<div class="snowflake large" style="left: 120px; animation-delay: 0.5s;"></div>' +
-                    '<div class="snowflake large" style="left: 170px; animation-delay: 0.7s;"></div>' +
-                    '<div class="snowflake large" style="left: 200px; animation-delay: 0.9s;"></div>' +
-                    '<div class="snowflake small" style="left: 40px; animation-delay: 0.5s;"></div>' +
-                    '<div class="snowflake small" style="left: 90px; animation-delay: 0.8s;"></div>' +
-                    '<div class="snowflake small" style="left: 130px; animation-delay: 0.9s;"></div>' +
-                    '<div class="snowflake small" style="left: 160px; animation-delay: 1s;"></div>' +
-                    '<div class="snowflake small" style="left: 220px; animation-delay: 0.1s;"></div>' +
-                    '<div class="snowman">' +
-                        '<!-- Snowman SVG code -->' +
-                    '</div>' +
-                '</div>';
-        } else if (weatherType === 'thunder') {
-            container.innerHTML = 
-                '<div class="cloud_thunder">' +
-                    '<div class="lightning"></div>' +
-                    '<div class="music_symbol"></div>' +
-                    '<div class="music_symbol" style="top: 70px; left: 140px;"></div>' +
-                    '<div class="music_symbol" style="top: 90px; left: 80px;"></div>' +
-                '</div>';
-        } else if (weatherType === 'sun') {
-            container.innerHTML = 
-                '<div class="cloud_sun">' +
-                    '<div class="sun"></div>' +
-                '</div>';
-        } else if (weatherType === 'unknown') {
-            container.innerHTML = `<div class="unknown">无法识别的天气类型: ${weatherText}</div>`;
-        } else if (weatherType === 'close') {
-            container.innerHTML = ''; // 关闭天气效果
-        }
-    }
-
-    window.showWeather = showWeather;
+    // 给按钮添加点击事件监听
+    document.getElementById('change-weather-button').addEventListener('click', function() {
+        weatherShown = false; // 重置标志变量，允许天气效果被更新
+        fetchLocationAndWeather(); // 再次获取天气数据并更新显示
+    });
 });
